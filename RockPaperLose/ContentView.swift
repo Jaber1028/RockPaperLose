4aeac97ce1679976f53774d8f470bf497bf2fdc9
Jacob Aberasturi <jacobaberasturi@gmail.com>
2023-01-19 20:18:34 -0500

    Inital UI and score evaluation
    Still need to implement shuffle after move, game over, alerts, and color overhaul

--- RockPaperLose/ContentView.swift
+++ RockPaperLose/ContentView.swift
@@ -7,15 +7,76 @@
 
 import SwiftUI
 
+struct MoveImageModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.system(size: 100))
+    }
+}
+
+extension View {
+    func moveImage() -> some View {
+        modifier(MoveImageModifier())
+    }
+}
+
 struct ContentView: View {
+                                  // Rock, Paper, Scissors
+    private let moves: [String] = ["mountain.2", "doc.fill", "scissors"]
+    private let winningMoves = ["doc.fill", "scissors", "mountain.2"]
+    private var initMove: String {
+        moves.randomElement() ?? "Rock"
+    }
+    @State private var currentMove = Int.random(in: 0...2)
+    @State private var winOrLose = Bool.random()
+    
+    // game ends in 10 turns
+    @State private var turn = 1
+    @State private var score = 0
+    
+    @State private var showResult = false
+    @State private var gameOver = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            VStack(alignment: .center, spacing: 20) {
+                Text("RockPaperLose?")
+                    .font(.largeTitle)
+                    .bold()
+                
+                Text("Turn Number: \(turn)")
+                
+                Spacer()
+                
+                Image(systemName: moves[currentMove]).moveImage()
+                Spacer()
+                Text("Goal: \(winOrLose ? "Win" : "Lose")!").font(.title)
+            
+                HStack {
+                    ForEach(moves.filter{$0 != moves[currentMove]},
+                            id: \.self) { choice in
+                        Button {
+                            checkIfScore(choice)
+                        } label: {
+                            Image(systemName: choice).moveImage()
+                        }
+                    }
+                }
+                Spacer()
+                Text("Score: \(score)").font(.title2)
+                Spacer()
+            }
         }
-        .padding()
+    }
+    func checkIfScore(_ choice: String) -> Void {
+        if winOrLose && choice == winningMoves[currentMove] || !winOrLose && choice != winningMoves[currentMove]{
+            score += 1
+        }
+        else {
+            // ignore for now
+        }
+        
+        turn += 1
     }
 }
 
