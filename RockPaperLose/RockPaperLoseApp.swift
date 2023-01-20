0f18e92c6ea5db78b99fef54d6c157f86f65763e
Jacob Aberasturi <jacobaberasturi@gmail.com>
2023-01-19 19:04:52 -0500

    Initial commit

---
+++ RockPaperLose/RockPaperLoseApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperLoseApp.swift
+//  RockPaperLose
+//
+//  Created by jacob aberasturi on 1/19/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperLoseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
